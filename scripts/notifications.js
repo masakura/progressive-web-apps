501f1e102889abf6c4a410b8f62365bddd75be36
Tomo Masakura <tomo.masakura@gmail.com>
2016-02-28 12:12:22 +0900

    ServiceWorker 版を追加

--- scripts/notifications.js
+++ scripts/notifications.js
@@ -7,15 +7,37 @@
     $('#permission').text('[' + permission + ']');
   });
 
+  // Notifications API の許可
   Notification.requestPermission(function(permission_) {
     permission = permission_;
     $('#permission').text(permission);
   });
 
+  // ServiceWorker の登録
+  var registration;
+  navigator.serviceWorker.register('scripts/worker.js')
+    .then(function(registration_) {
+      registration = registration_;
+    });
+
   $(document).on('click', '#notify', function() {
     if (permission === 'granted') {
       var notification = new Notification('通知', {
         body: 'メールが来てません!',
+        icon: 'images/icon.png',
+        vibrate: [300, 100, 300, 100, 300]
+      });
+      notification.onclick = function() {
+        notification.close();
+        window.open(window.location, '_blank');
+      };
+    }
+  });
+
+  $(document).on('click', '#notify-serviceworker', function() {
+    if (permission === 'granted') {
+      var notification = registration.showNotification('通知', {
+        body: 'メールが来てません! (ServiceWorker 経由)',
         icon: 'images/icon.png',
         vibrate: [300, 100, 300, 100, 300]
       });
