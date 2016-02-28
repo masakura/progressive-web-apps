86bfb5bfe2d606ec224d54efcdba77ea4621866a
Tomo Masakura <tomo.masakura@gmail.com>
2016-02-28 13:23:27 +0900

    チャットの発言を通知してみた

--- scripts/chat.js
+++ scripts/chat.js
@@ -10,12 +10,41 @@
     $('#permission').text('[' + permission + ']');
   });
 
+  // Notifications API の許可
   Notification.requestPermission(function(permission_) {
     permission = permission_;
     $('#permission').text(permission);
   });
 
+  // ServiceWorker の登録
+  var registration;
+  navigator.serviceWorker.register('scripts/worker-chat.js')
+    .then(function(registration_) {
+      registration = registration_;
+    });
+
   datastore.on('send', function(data) {
-    console.log(data);
+    var text = data.value.text;
+
+    $('<li class="list-group-item"></li>')
+      .text(text)
+      .prependTo('#messages');
+
+    if (permission === 'granted') {
+      registration.showNotification('通知', {
+        body: text,
+        icon: 'images/icon.png',
+        vibrate: [300, 100, 300, 100, 300]
+      });
+    }
+  });
+
+  $(document).on('click', '#send', function() {
+    var text = $('#input').val();
+    $('#input').val('');
+
+    datastore.send({
+      text: text
+    });
   });
 })();
